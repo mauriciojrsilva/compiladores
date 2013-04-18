73b6dbf84c9436be1c455c014d3cd999518d53fa
Lucas Lazari <lucaslazari86@gmail.com>
2013-04-17 22:39:25 -0300

    ajustes...

--- etapa2/main.c
+++ etapa2/main.c
@@ -1,32 +1,21 @@
 /*
-   
-   Etapa 1 - Analisador Léxico
-   
-   Lucas Lazari da Silva
-   Diogo Dadalt
-   Mauricio Jaspe 
-   
+   main.c
+
+   Arquivo principal do analisador sintático.
 */
 #include <stdio.h>
 #include "tokens.h"
 
 extern FILE *yyin;
 
-int running;
+void yyerror (char const *mensagem)
+{
+  fprintf (stderr, "%s\n", mensagem);
+}
 
 int main (int argc, char **argv)
 {
-	// inicia a hash table	
-	initMe();
-	
-  	int token = yylex();
-	
-	while (running) {
-    	printf ("token  %d - linha %d\n", token, getLineNumber());
-    	token = yylex();
-  	}
-
-  	hash_print();
-
-  	return 0;
+  int resultado = yyparse();
+  return resultado;
 }
+
---
+++ etapa2/parser.y
@@ -0,0 +1,13 @@
+%{
+#include <stdio.h>
+%}
+
+/* Declaração dos tokens da gramática da Linguagem K */
+%token NUM
+
+%%
+ /* Regras (e ações) da gramática da Linguagem K */
+
+s:
+
+%%

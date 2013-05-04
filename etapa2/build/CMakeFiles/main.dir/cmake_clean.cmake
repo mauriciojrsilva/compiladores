96730cae4b30b808799bed94770c1a89b19a92ce
Lucas Lazari <lucaslazari86@gmail.com>
2013-05-04 16:08:13 -0300

    inicializando diretório para a etapa 3

---
+++ etapa2/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+FILE(REMOVE_RECURSE
+  "scanner.c"
+  "parser.c"
+  "parser.h"
+  "CMakeFiles/main.dir/main.c.o"
+  "CMakeFiles/main.dir/scanner.c.o"
+  "CMakeFiles/main.dir/parser.c.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)

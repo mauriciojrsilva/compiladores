ebe6918010b83c869e83951d8dc87107989d80bf
Lucas Lazari <lucaslazari86@gmail.com>
2013-05-09 23:32:57 -0300

    sh para testar automaticamente as entradas

--- etapa3/teste.sh
+++ etapa3/teste.sh
@@ -1,42 +1,20 @@
-echo "CERTOS"
-cat ex/certo01.txt | ./build/main
-cat ex/certo02.txt | ./build/main
-cat ex/certo03.txt | ./build/main
-cat ex/certo04.txt | ./build/main
-cat ex/certo05.txt | ./build/main
-cat ex/certo06.txt | ./build/main
-cat ex/certo07.txt | ./build/main
-cat ex/certo08.txt | ./build/main
-cat ex/certo09.txt | ./build/main
-cat ex/certo10.txt | ./build/main
-cat ex/certo11.txt | ./build/main
-cat ex/certo12.txt | ./build/main
-cat ex/certo13.txt | ./build/main
-cat ex/certo14.txt | ./build/main
-cat ex/certo15.txt | ./build/main
-cat ex/certo16.txt | ./build/main
-cat ex/certo17.txt | ./build/main
-cat ex/certo18.txt | ./build/main
-cat ex/certo19.txt | ./build/main
-cat ex/certo20.txt | ./build/main
-echo "ERRADOS"
-cat ex/errado01.txt | ./build/main
-cat ex/errado02.txt | ./build/main
-cat ex/errado03.txt | ./build/main
-cat ex/errado04.txt | ./build/main
-cat ex/errado05.txt | ./build/main
-cat ex/errado06.txt | ./build/main
-cat ex/errado07.txt | ./build/main
-cat ex/errado08.txt | ./build/main
-cat ex/errado09.txt | ./build/main
-cat ex/errado10.txt | ./build/main
-cat ex/errado11.txt | ./build/main
-cat ex/errado12.txt | ./build/main
-cat ex/errado13.txt | ./build/main
-cat ex/errado14.txt | ./build/main
-cat ex/errado15.txt | ./build/main
-cat ex/errado16.txt | ./build/main
-cat ex/errado17.txt | ./build/main
-cat ex/errado18.txt | ./build/main
-cat ex/errado19.txt | ./build/main
-cat ex/errado20.txt | ./build/main
+./build/main ex/certo01.txt ex/saida01.txt
+./build/main ex/certo02.txt ex/saida02.txt
+./build/main ex/certo03.txt ex/saida03.txt
+./build/main ex/certo04.txt ex/saida04.txt
+./build/main ex/certo05.txt ex/saida05.txt
+./build/main ex/certo06.txt ex/saida06.txt
+./build/main ex/certo07.txt ex/saida07.txt
+./build/main ex/certo08.txt ex/saida08.txt
+./build/main ex/certo09.txt ex/saida09.txt
+./build/main ex/certo10.txt ex/saida10.txt
+./build/main ex/certo11.txt ex/saida11.txt
+./build/main ex/certo12.txt ex/saida12.txt
+./build/main ex/certo13.txt ex/saida13.txt
+./build/main ex/certo14.txt ex/saida14.txt
+./build/main ex/certo15.txt ex/saida15.txt
+./build/main ex/certo16.txt ex/saida16.txt
+./build/main ex/certo17.txt ex/saida17.txt
+./build/main ex/certo18.txt ex/saida18.txt
+./build/main ex/certo19.txt ex/saida19.txt
+./build/main ex/certo20.txt ex/saida20.txt

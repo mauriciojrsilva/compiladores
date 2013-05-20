4f340a6fe104f51dd604ae3396357c1a5ef7318a
diogodadalt <diogodadalt@yahoo.com.br>
2013-05-20 02:06:57 -0300

    Arquivos iniciais etapa 4

---
+++ etapa4/ast.c
@@ -0,0 +1,372 @@
+#include "ast.h"
+
+FILE *yyout;
+
+// funcao para a criação de um novo nodo
+AST* criaAST(int tipo, HASH_ELEMENT* simbolo, AST** filhos, int numFilhos) {
+	AST *nodo = (AST*) malloc(sizeof(AST));	
+	nodo->tipo = tipo;
+	nodo->numFilhos = numFilhos;
+	nodo->simbolo = simbolo;
+	nodo->filhos	= filhos;	
+	return nodo;
+}
+
+AST** criaNodos(AST* f1, AST* f2, AST* f3, AST* f4, int numFilhos) {
+	int index = 0;
+	AST** nodos = (AST**)calloc(numFilhos, sizeof(AST*));
+	criaNodo(nodos, f1, &index);
+	criaNodo(nodos, f2, &index);
+	criaNodo(nodos, f3, &index);
+	criaNodo(nodos, f4, &index);
+	return nodos;
+}
+
+void criaNodo(AST** filhos, AST* filho, int* index) {
+	if (filho != NULL) {
+		filhos[(*index)] = filho;
+		(*index)++;
+	}
+}
+
+void astPrintNodo(AST *nodo) {
+        if (nodo == 0)
+                return;
+       
+        printf("AST(");
+       
+        switch(nodo->tipo) {
+                case AST_SYMBOL: printf("Simbolo na hash");
+                        break;
+                case AST_OP_SUM: printf("Operacao +");
+                        break;
+                case AST_OP_SUB: printf("Operacao -");
+                        break;
+                case AST_OP_MUL: printf("Operacao *");
+                        break;
+                case AST_OP_DIV: printf("Operacao /");
+                        break;
+                case AST_OP_LES: printf("Operacao <");
+                        break;
+                case AST_OP_GRE: printf("Operacao >");
+                        break;
+                case AST_OP_LE: printf("Operacao LE");
+                        break;
+                case AST_OP_GE: printf("Operacao GE");
+                        break;
+                case AST_OP_EQ: printf("Operacao EQ");
+                        break;
+                case AST_OP_NE: printf("Operacao NE");
+                        break;
+                case AST_OP_AND: printf("Operacao AND");
+                        break;
+                case AST_OP_OR: printf("Operacao OR");
+                        break;
+                case AST_LIST_E: printf("Lista de expressoes");
+                        break;
+                case AST_IF: printf("Se");
+                        break;
+                case AST_WHILE: printf("Enquanto");
+                        break;
+                case AST_RET: printf("Retorna");
+                        break;
+                case AST_INP: printf("Entrada");
+                        break;
+                case AST_OUT: printf("Saida");
+                        break;
+                case AST_ATR_VAR: printf("Atribuicao variavel");
+                        break;
+                case AST_ATR_VEC: printf("Atribuicao vetor");
+                        break;
+                case AST_CHAM_F: printf("Chamada de funcao");
+                        break;
+                case AST_SEQ: printf("Sequencia de comandos");
+                        break;
+                case AST_PARAM: printf("Parametro");
+                        break;
+                case AST_T_INT: printf("Tipo inteiro");
+                        break;
+                case AST_T_FLO: printf("Tipo flutuante");
+                        break;
+                case AST_T_BOO: printf("Tipo booleano");
+                        break;
+                case AST_T_CHA: printf("Tipo caracter");
+                        break;
+                case AST_LIST_P: printf("Lista de parametros");
+                        break;  
+                case AST_DEF_F: printf("Definicao de funcao");
+                        break;
+                case AST_DECL_VEC: printf("Declaracao de vetor");
+                        break;
+                case AST_VEC_SIZE: printf("Tamanho do vetor");
+                        break;
+                case AST_DECL_VAR: printf("Declaracao de variavel");
+                        break;
+                case AST_DECL_GL: printf("Declaracao global");
+                        break;
+                case AST_PROG: printf("Programa");
+                        break;
+                case AST_BLO_COM: printf("Bloco de comandos");
+                        break;
+                case AST_COM: printf("Comando");
+                        break;
+                case AST_EMPTY: printf("Empty");
+                        break;
+                default: printf("UNKNOWN");
+                        break;
+        }
+       
+        if (nodo->simbolo != 0)
+                printf(", %s", nodo->simbolo->text);
+               
+        printf(");\n");
+}
+
+
+void imprimeArvore_aux(AST *raiz, int nivel) {
+  if (raiz == 0)
+  	return;
+ 
+  int i;
+  for (i = 0; i < nivel; ++i)	{
+  	printf("  ");
+  }
+
+  astPrintNodo(raiz);
+ 
+  for(i = 0; i < raiz->numFilhos; i++) {
+  	imprimeArvore_aux(raiz->filhos[i], nivel+1);
+  }
+}
+
+void imprimeArvore(AST *raiz) {
+	imprimeArvore_aux(raiz, 1);
+}
+
+void astImprimeArvoreArquivo(AST* nodo) {
+  if (nodo == 0)
+  	return;
+
+  int i;
+
+  switch(nodo->tipo) {
+    case AST_SYMBOL: 
+    	fprintf(yyout, "%s", nodo->simbolo->text);
+    	break;
+    case AST_SYMBOL_VEC: 
+    	fprintf(yyout, "%s", nodo->simbolo->text);
+    	fprintf(yyout, "[");
+			astImprimeArvoreArquivo(nodo->filhos[0]);
+    	fprintf(yyout, "]");
+    	break;
+    case AST_OP_SUM:
+    	if (nodo->numFilhos == 1) {
+    		fprintf(yyout, " + ");
+    		astImprimeArvoreArquivo(nodo->filhos[0]);
+    	} else if (nodo->numFilhos == 2) {
+    		astImprimeArvoreArquivo(nodo->filhos[0]);
+      	fprintf(yyout, " + ");
+      	astImprimeArvoreArquivo(nodo->filhos[1]);
+    	}      
+      break;
+    case AST_OP_SUB: 
+      if (nodo->numFilhos == 1) {
+    		fprintf(yyout, " - ");
+    		astImprimeArvoreArquivo(nodo->filhos[0]);
+    	} else if (nodo->numFilhos == 2) {
+    		astImprimeArvoreArquivo(nodo->filhos[0]);
+      	fprintf(yyout, " - ");
+      	astImprimeArvoreArquivo(nodo->filhos[1]);
+    	}      
+      break;
+    case AST_OP_MUL: 
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, " * ");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      break;
+    case AST_OP_DIV: 
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, " / ");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      break;
+    case AST_OP_LES: 
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, " < ");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      break;
+    case AST_OP_GRE: 
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, " > ");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      break;
+    case AST_OP_LE: 
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, " <= ");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      break;
+    case AST_OP_GE: 
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, " >= ");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      break;
+    case AST_OP_EQ: 
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, " == ");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      break;
+    case AST_OP_NE: 
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, " != ");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      break;
+    case AST_OP_AND: 
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, " && ");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      break;
+    case AST_OP_OR: 
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, " || ");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      break;
+    case AST_LIST_E: 
+    	fprintf(yyout, " ");
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      if(nodo->filhos[1] != 0) {
+        fprintf(yyout, ",");
+        astImprimeArvoreArquivo(nodo->filhos[1]);
+      }
+      break;
+    case AST_IF: 
+    	fprintf(yyout, "se (");
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, ")\nentao ");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      if(nodo->filhos[2] != 0) {
+        fprintf(yyout, "\nsenao ");
+        astImprimeArvoreArquivo(nodo->filhos[2]);
+      }
+      break;
+    case AST_WHILE:
+    	fprintf(yyout, "enquanto (");
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, ")\n");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      break;
+    case AST_RET: 
+    	fprintf(yyout, "retorna ");
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, "; ");
+      break;
+    case AST_INP: 
+    	fprintf(yyout, "entrada %s", nodo->simbolo->text);
+    	fprintf(yyout, "; ");
+      break;
+    case AST_OUT: 
+    	fprintf(yyout, "saida ");
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, "; ");
+      break;
+    case AST_ATR_VAR: 
+    	fprintf(yyout, "%s = ", nodo->simbolo->text);
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, "; ");
+      break;
+    case AST_ATR_VEC: 
+    	fprintf(yyout, "%s[", nodo->simbolo->text);
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, "] = ");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      fprintf(yyout, "; ");
+      break;
+    case AST_CHAM_F: 
+    	fprintf(yyout, "%s (", nodo->simbolo->text);
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, ")");
+      break;
+    case AST_SEQ: fprintf(yyout, "\n");
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      if(nodo->filhos[1] != 0) {        
+        astImprimeArvoreArquivo(nodo->filhos[1]);
+      }
+      break;
+    case AST_PARAM: 
+    	fprintf(yyout, "%s: ", nodo->simbolo->text);
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      break;
+    case AST_T_INT: 
+    	fprintf(yyout, "inteiro");
+      break;
+    case AST_T_FLO: 
+    	fprintf(yyout, "flutuante");
+      break;
+    case AST_T_BOO: 
+    	fprintf(yyout, "booleano");
+      break;
+    case AST_T_CHA: 
+    	fprintf(yyout, "caractere");
+      break;
+    case AST_T_STR: 
+    	fprintf(yyout, "cadeia");
+      break;
+    case AST_LIST_P: 
+    	astImprimeArvoreArquivo(nodo->filhos[0]);
+      if(nodo->filhos[1] != 0) {
+        fprintf(yyout, ", ");
+        astImprimeArvoreArquivo(nodo->filhos[1]);
+      }
+      break;
+    case AST_HEADER: 
+    	fprintf(yyout, "\n%s: ", nodo->simbolo->text);
+    	astImprimeArvoreArquivo(nodo->filhos[0]);
+    	fprintf(yyout, "(");
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      fprintf(yyout, ")\n");
+      break;
+    case AST_DECL_LOC:
+    	astImprimeArvoreArquivo(nodo->filhos[0]);
+    	if(nodo->filhos[1] != 0) {    		
+    		astImprimeArvoreArquivo(nodo->filhos[1]);
+    	}
+      break;
+    case AST_DEF_F: 
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      astImprimeArvoreArquivo(nodo->filhos[2]);
+      break;
+    case AST_DECL_VEC: 
+    	fprintf(yyout, "%s: ", nodo->simbolo->text);
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      astImprimeArvoreArquivo(nodo->filhos[1]);
+      fprintf(yyout, "; ");
+      break;
+    case AST_VEC_SIZE: 
+    	fprintf(yyout, " [%s]", nodo->simbolo->text);
+      break;
+    case AST_DECL_VAR: 
+    	fprintf(yyout, "%s: ", nodo->simbolo->text);
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, "; ");
+      break;
+    case AST_DECL_GL: 
+    	astImprimeArvoreArquivo(nodo->filhos[0]);
+      break;
+    case AST_PROG: 
+    	fprintf(yyout, "\n");
+      for(i = 0; i < nodo->numFilhos; i++)
+      	astImprimeArvoreArquivo(nodo->filhos[i]);
+      break;
+    case AST_BLO_COM: 
+    	fprintf(yyout, "{");
+      astImprimeArvoreArquivo(nodo->filhos[0]);
+      fprintf(yyout, "\n}");
+      break;
+    case AST_COM: 
+    	astImprimeArvoreArquivo(nodo->filhos[0]);
+      break;
+    case AST_EMPTY: 
+    	fprintf(yyout, "");
+      break;
+    default: break;
+  }
+}
---
+++ etapa4/teste.sh
@@ -0,0 +1,100 @@
+./build/main ex/certo01.txt ex/saida01.txt
+./build/main ex/saida01.txt ex/final01.txt
+
+./build/main ex/certo02.txt ex/saida02.txt
+./build/main ex/saida02.txt ex/final02.txt
+
+./build/main ex/certo03.txt ex/saida03.txt
+./build/main ex/saida03.txt ex/final03.txt
+
+./build/main ex/certo04.txt ex/saida04.txt
+./build/main ex/saida04.txt ex/final04.txt
+
+./build/main ex/certo05.txt ex/saida05.txt
+./build/main ex/saida05.txt ex/final05.txt
+
+./build/main ex/certo06.txt ex/saida06.txt
+./build/main ex/saida06.txt ex/final06.txt
+
+./build/main ex/certo07.txt ex/saida07.txt
+./build/main ex/saida07.txt ex/final07.txt
+
+./build/main ex/certo08.txt ex/saida08.txt
+./build/main ex/saida08.txt ex/final08.txt
+
+./build/main ex/certo09.txt ex/saida09.txt
+./build/main ex/saida09.txt ex/final09.txt
+
+./build/main ex/certo10.txt ex/saida10.txt
+./build/main ex/saida10.txt ex/final10.txt
+
+./build/main ex/certo11.txt ex/saida11.txt
+./build/main ex/saida11.txt ex/final11.txt
+
+./build/main ex/certo12.txt ex/saida12.txt
+./build/main ex/saida12.txt ex/final12.txt
+
+./build/main ex/certo13.txt ex/saida13.txt
+./build/main ex/saida13.txt ex/final13.txt
+
+./build/main ex/certo14.txt ex/saida14.txt
+./build/main ex/saida14.txt ex/final14.txt
+
+./build/main ex/certo15.txt ex/saida15.txt
+./build/main ex/saida15.txt ex/final15.txt
+
+./build/main ex/certo16.txt ex/saida16.txt
+./build/main ex/saida16.txt ex/final16.txt
+
+./build/main ex/certo17.txt ex/saida17.txt
+./build/main ex/saida17.txt ex/final17.txt
+
+./build/main ex/certo18.txt ex/saida18.txt
+./build/main ex/saida18.txt ex/final18.txt
+
+./build/main ex/certo19.txt ex/saida19.txt
+./build/main ex/saida19.txt ex/final19.txt
+
+./build/main ex/certo20.txt ex/saida20.txt
+./build/main ex/saida20.txt ex/final20.txt
+
+echo "\n1\n"
+sdiff ex/saida01.txt ex/final01.txt
+echo "\n2\n"
+sdiff ex/saida02.txt ex/final02.txt
+echo "\n3\n"
+sdiff ex/saida03.txt ex/final03.txt
+echo "\n4\n"
+sdiff ex/saida04.txt ex/final04.txt
+echo "\n5\n"
+sdiff ex/saida05.txt ex/final05.txt
+echo "\n6\n"
+sdiff ex/saida06.txt ex/final06.txt
+echo "\n7\n"
+sdiff ex/saida07.txt ex/final07.txt
+echo "\n8\n"
+sdiff ex/saida08.txt ex/final08.txt
+echo "\n9\n"
+sdiff ex/saida09.txt ex/final09.txt
+echo "\n10\n"
+sdiff ex/saida10.txt ex/final10.txt
+echo "\n11\n"
+sdiff ex/saida11.txt ex/final11.txt
+echo "\n12\n"
+sdiff ex/saida12.txt ex/final12.txt
+echo "\n13\n"
+sdiff ex/saida13.txt ex/final13.txt
+echo "\n14\n"
+sdiff ex/saida14.txt ex/final14.txt
+echo "\n15\n"
+sdiff ex/saida15.txt ex/final15.txt
+echo "\n16\n"
+sdiff ex/saida16.txt ex/final16.txt
+echo "\n17\n"
+sdiff ex/saida17.txt ex/final17.txt
+echo "\n18\n"
+sdiff ex/saida18.txt ex/final18.txt
+echo "\n19\n"
+sdiff ex/saida19.txt ex/final19.txt
+echo "\n20\n"
+sdiff ex/saida20.txt ex/final20.txt

469f5a79d15e7ee7602e06d0e58c9e9c9b730fc4
Lucas Lazari <lucaslazari86@gmail.com>
2013-05-21 00:47:41 -0300

    implementada função semantica para checar declarações de identificadores

--- etapa4/semantica.c
+++ etapa4/semantica.c
@@ -1,9 +1,42 @@
 #include "astree.h"
 
-/* Funções principais da análise semântica, chamadas no início da análise sintática*/
 void verificaDeclaracoes(AST *raiz) {
+	if (raiz == 0) return;
 	
-
+	// deve-se verificar as declarações somente se o nodo da AST for uma declaração (de variável, vetor, função ou parâmetro de função)
+	if (raiz->tipo == AST_DECL_VAR || raiz->tipo == AST_DECL_VEC || raiz->tipo == AST_DEF_F || raiz->tipo == AST_PARAM) {
+	
+		// verifica se foi colocado o nome do identificador
+		if (raiz->simbolo == 0) {
+    	printf("Linha %d: Declaração de identificador: faltando o nome do identificador.\n", raiz->linha);
+    } else {
+			
+			// inicialmente o elemento da hashtable retorna um token de identificador. se ele não retornar este token, é porque o símbolo já foi definido
+			if (raiz->simbolo->token != SIMBOLO_IDENTIFICADOR) {
+				printf("Linha %d: Identificador %s já definido.\n", raiz->linha, raiz->simbolo->text);
+			}
+			
+			// define o tipo do símbolo
+			switch(raiz->tipo) { 
+				case AST_DECL_VAR: 
+					raiz->simbolo->token = SIMBOLO_VARIAVEL;
+					break;
+				case AST_DECL_VEC: 
+					raiz->simbolo->token = SIMBOLO_VETOR;
+					break;
+				case AST_DEF_F: 
+					raiz->simbolo->token = SIMBOLO_FUNCAO;
+					break;
+				case AST_PARAM: 
+					raiz->simbolo->token = SIMBOLO_PARAM;
+					break;
+			}	
+	}
+	
+	// chama recursivamente os filhos para continuar a verificação de declarações...
+	for (int i = 0; i < MAX_FILHOS; i++) {
+		verificaDeclaracoes(raiz->filhos[i]):	
+	}	
 }
 
 void verificaUtilizacao(AST *raiz) {
@@ -16,7 +49,9 @@ void verificaDados(AST *raiz) {
 
 }
 
-/* Funções que auxiliam as funções principais acima */
+
+
+
 void verificaChamadaFuncao() { 
 	
 }

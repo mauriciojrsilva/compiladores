3a0731a42ce1555f5522bc337eb63f1b0baeed00
diogodadalt <diogodadalt@yahoo.com.br>
2013-06-10 02:44:08 -0300

    Refactoring to C++

--- etapa4/src/tree/vectordeclarationnode.cpp
+++ etapa4/src/tree/vectordeclarationnode.cpp
@@ -1,6 +1,19 @@
 #include "vectordeclarationnode.h"
+#include <stdio.h>
+#include <sstream>
 
 VectorDeclarationNode::VectorDeclarationNode(const std::string &vectorName, Common::DataType dataType, int size):
   Node("Declaracao de vetor"), vectorName(vectorName), dataType(dataType), size(size) {}
 
-void VectorDeclarationNode::printSourceCode() {}
+void VectorDeclarationNode::printSourceCode() {
+  std::stringstream sizeStr;
+  sizeStr << this->size;
+
+  fprintf(this->flexOut, "%s", this->vectorName.c_str());
+  fprintf(this->flexOut, "%s", ": ");
+  fprintf(this->flexOut, "%s", this->dataTypeToString(this->dataType).c_str());
+  fprintf(this->flexOut, "%s", "[");
+  fprintf(this->flexOut, "%s", sizeStr.str().c_str());
+  fprintf(this->flexOut, "%s", "]");
+  fprintf(this->flexOut, "%s", ";\n");
+}
